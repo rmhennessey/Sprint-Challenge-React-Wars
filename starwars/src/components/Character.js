211fa9337445be4ce691e36d07a9c84d39d21f7f
RMH <rory.hennessey@gmail.com>
2019-02-15 13:27:43 -0500

    simplified my components from 3 to 2, so there is just character.js and characterlist.js

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,29 +1,57 @@
 import React from "react";
-import ContentCard from "./ContentCard";
 
-
-const Character = props => {
-    return (
-        <div className = "character" >
-        <ContentCard name = {props.name} 
-                    birthYear = {props.birthYear}
-                    created = {props.created} 
-                    edited = {props.edited}
-                    eyeColor = {props.eyeColor}
-                    gender = {props.gender}
-                    hair = {props.hair}
-                    height = {props.height}
-                    homeworld = {props.homeworld}
-                    mass = {props.mass}
-                    skinColor = {props.skinColor}
-                    url = {props.url}
-                    films = {props.films}
-                    species = {props.species}
-                    starships = {props.starships}
-                    vehicles = {props.vehicles}
-                    />
+const Character  = props => {
+  return (
+    <div className="character">
+        <h3>
+            <strong>Name:</strong> {props.name}
+        </h3>
+        <p>
+            <strong>Birth Year:</strong> {props.birthYear}
+        </p>
+        <p>
+            <strong>Created:</strong> {props.created}
+        </p>
+        <p>
+            <strong>Edited:</strong> {props.edited}
+        </p>
+        <p>
+            <strong>Eye color:</strong> {props.eyeColor}
+        </p>
+        <p>
+            <strong>Gender:</strong> {props.gender}
+        </p>
+        <p>
+            <strong>Hair color:</strong> {props.hair}
+        </p>
+        <p>
+            <strong>Height:</strong> {props.height}
+        </p>
+        <p>
+            <strong>Homeworld:</strong> {props.homeworld}
+        </p>
+        <p>
+            <strong>Mass:</strong> {props.mass}
+        </p>
+        <p>
+            <strong>Skin Color:</strong> {props.skinColor}
+        </p>
+        <p>
+            <strong>URL:</strong> {props.url}
+        </p>
+        <p>
+            <strong>Films:</strong> {props.films}
+        </p>
+        <p>
+            <strong>Species:</strong> {props.species}
+        </p>
+        <p>
+            <strong>Starships:</strong> {props.starships}
+        </p>
+        <p>
+            <strong>Vehicles:</strong> {props.vehicles}
+        </p>
         </div>
-    )
-}
-
+  );
+};
 export default Character;
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,33 +1,34 @@
 import React from "react";
 import Character from "./Character";
+//import ContentCard from "./ContentCard";
 
 import "./StarWars.css";
 
 const CharacterList = props => {
-    return props.charList.map(character => { 
-        return (
-            <div className = "characterCard">
-            <Character 
-                name = {character.name}
-                birthYear = {character.birth_year}
-                created = {character.created} 
-                edited = {character.edited}
-                eyeColor = {character.eye_color}
-                gender = {character.gender}
-                hair = {character.hair_color}
-                height = {character.height}
-                homeworld = {character.homeworld}
-                mass = {character.mass}
-                skinColor = {character.skin_color}
-                url = {character.url}
-                films = {character.films[0]}
-                species = {character.species[0]}
-                starships = {character.starships[0]}
-                vehicles = {character.vehicles[0]}
-                />
-            </div>
-        ) 
-    })
-}
+  return props.charList.map(character => {
+    return (
+      <div className="characterCard">
+        <Character
+            name={character.name}
+            birthYear={character.birth_year}
+            created={character.created}
+            edited={character.edited}
+            eyeColor={character.eye_color}
+            gender={character.gender}
+            hair={character.hair_color}
+            height={character.height}
+            homeworld={character.homeworld}
+            mass={character.mass}
+            skinColor={character.skin_color}
+            url={character.url}
+            films={character.films[0]}
+            species={character.species[0]}
+            starships={character.starships[0]}
+            vehicles={character.vehicles[0]}
+        />
+      </div>
+    );
+  });
+};
 
-export default CharacterList; 
+export default CharacterList;
